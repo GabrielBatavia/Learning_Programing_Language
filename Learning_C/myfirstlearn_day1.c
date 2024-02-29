bad1c93dea251995b481e8321169605c4a045b61
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-02-29 22:45:20 +0700

    Learn how to comment in c

--- Learning_C/myfirstlearn_day1.c
+++ Learning_C/myfirstlearn_day1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello Sekai!\nI am learning C.\nAnd it is awesome!");
+  printf("Hello Sekai!\n\tI am learning \"C.\nAnd it is awesome!"); //aku komen
   return 0;
 }
 
