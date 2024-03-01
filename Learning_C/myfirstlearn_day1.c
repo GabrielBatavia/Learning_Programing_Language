e46248e71eec6b8f1ece55ed2bccf245db60105e
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-01 18:45:53 +0700

    Learn how to change variabel

--- Learning_C/myfirstlearn_day1.c
+++ Learning_C/myfirstlearn_day1.c
@@ -3,6 +3,8 @@
 int main() {
   printf("Hello Sekai!\n\tI am learning \"C.\nAnd it is awesome!\n"); //aku komen
 
+  //Learn variable
+
   int myNum = 15;
   printf("%d\n", myNum);
 
@@ -10,7 +12,13 @@ int main() {
   printf("%f\n", myFloat);
 
   char myLetter = 'D';
-  printf("My number is %d and my letter is %c", myNum, myLetter);
+  printf("My number is %d and my letter is %c\n", myNum, myLetter);
+
+  printf("%c\n", myLetter);
+
+  myNum = 10;  // Now myNum is 10
+
+  printf("%d\n", myNum);
 
 
   return 0;
