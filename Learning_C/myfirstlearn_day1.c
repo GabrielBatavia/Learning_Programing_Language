20783f07612936fb8ca2be116a9eaeea23de4726
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-01 19:03:46 +0700

    Learn other way to declare multi variable

--- Learning_C/myfirstlearn_day1.c
+++ Learning_C/myfirstlearn_day1.c
@@ -39,12 +39,16 @@ int main() {
   int x = 5;
   int y = 6;
   int sum = x + y;
-  printf("%d", sum);
+  printf("%d\n", sum);
 
   //declare multi variable
 
   int a = 5, b = 6, c = 50;
-  printf("%d", a + b + c);
+  printf("%d\n", a + b + c);
+
+  int d, f, g;
+  d = f = g = 50;
+  printf("%d\n", d + f + g);
 
   return 0;
 }
