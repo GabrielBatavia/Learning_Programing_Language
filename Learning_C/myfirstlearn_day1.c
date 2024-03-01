a04144f534fb43ba89efb28dc1b59a92a8548340
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-01 18:34:54 +0700

    Learning make variabel in c

--- Learning_C/myfirstlearn_day1.c
+++ Learning_C/myfirstlearn_day1.c
@@ -2,9 +2,14 @@
 
 int main() {
   printf("Hello Sekai!\n\tI am learning \"C.\nAnd it is awesome!"); //aku komen
+
+  int myNum = 15;
+  printf("%d", myNum);
+
   return 0;
 }
 
 /*
 jsafiishoooooo komennnnnn
 */
+
