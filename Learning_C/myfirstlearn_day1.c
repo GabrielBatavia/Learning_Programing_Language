c606448a2453ae9be31c739eff36717d6aca15fe
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-05 22:18:05 +0700

    learn use do looping

--- Learning_C/myfirstlearn_day1.c
+++ Learning_C/myfirstlearn_day1.c
@@ -185,6 +185,15 @@ int main() {
   }
   while (i < 5);
 
+  int countdown = 3;
+
+  while (countdown > 0) {
+    printf("%d\n", countdown);
+    countdown--;
+  }
+
+  printf("Happy New Year!!\n");
+
     return 0;
   }
 
