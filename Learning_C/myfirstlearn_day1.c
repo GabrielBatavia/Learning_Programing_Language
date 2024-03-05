5305eea95805ef0447e758dc4abe0b6df359bdcb
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-05 22:44:26 +0700

    learn nested loop

--- Learning_C/myfirstlearn_day1.c
+++ Learning_C/myfirstlearn_day1.c
@@ -210,6 +210,18 @@ int main() {
     printf("%d\n", i);
   }
 
+  int j;
+
+  // Outer loop
+  for (i = 1; i <= 2; ++i) {
+    printf("Outer: %d\n", i);  // Executes 2 times
+
+    // Inner loop
+    for (j = 1; j <= 3; ++j) {
+      printf(" Inner: %d\n", j);  // Executes 6 times (2 * 3)
+    }
+  }
+
 
     return 0;
   }
