b7cdd4d3fb076fd98c946ea8ebc12a32dc79d11a
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-08 09:13:57 +0700

    learn type data and pow

--- Learning_Python/learnPython1.py
+++ Learning_Python/learnPython1.py
@@ -18,3 +18,36 @@ x = teks.count("semuanya")
 print(x)
 
 print(100//20)
+
+#int() -> Digunakan untuk mengubah nilai yang ditentukan menjadi bilangan bulat. Contohnya:
+x = int("12")
+y = int(12.5)
+
+print(x)
+print(y)
+
+#output
+#12
+#12
+
+#pow() -> Digunakan untuk mendapatkan nilai pangkat dari suatu bilangan. Contohnya:
+x = pow(3, 3) # 3 pangkat 3
+y = pow(3, -3) # 3 pangkat -3
+
+print(x)
+print(y)
+
+#output
+#27
+#0.015625
+
+#str() -> Digunakan untuk mengubah sebuah objek menjadi string. Contohnya:
+x = str(12)
+y = str(12.5)
+
+print(type(x))
+print(type(y))
+
+#output
+#<class 'str'>
+#<class 'str'>
