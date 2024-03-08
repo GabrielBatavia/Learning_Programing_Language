1fc6c68834a77b46ddb79878cac413ac3214ee36
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-08 09:04:40 +0700

    learn about islower

--- Learning_Python/learnPython1.py
+++ Learning_Python/learnPython1.py
@@ -4,3 +4,10 @@ print(1+2)
 teks = "halo, nama saya, budi"
 x = teks.split(", ")
 print(x)
+
+#islower() -> Digunakan untuk mengecek apakah semua element dalam string adalah huruf kecil, akan mengembalikan True jika iya, dan False jika tidak
+a = "HaLo"
+b = "halo"
+
+print(a.islower())
+print(b.islower())
