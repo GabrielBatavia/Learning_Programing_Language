9b2bf978fa7a6dfb9e2c4214935d282b7124a7c8
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-10 15:11:24 +0700

    learn more about function try make 1 example

--- Learning_Python/learnpython_intermediate.py
+++ Learning_Python/learnpython_intermediate.py
@@ -1,4 +1,11 @@
-def redmi():
+def redmi(namaProduk, harga):
     print("Selamat Anda Telah Membeli")
     
-redmi()
+    print("Handphone yang anda beli : " + namaProduk)
+    print("Harga yang anda beli     : " + str(harga))
+    return(namaProduk, harga)
+
+namaProduk = input("Masukkan nama Handphone yang anda ingin beli : ")
+harga = float(input("Masukkan harga Handphone yang anda ingin dibeli   : "))
+
+redmi(namaProduk, harga)
