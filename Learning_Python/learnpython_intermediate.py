9f30028bcd2bb5f4977bdd59702aae7ec4a1442d
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-10 15:18:51 +0700

    learn loopin in python

--- Learning_Python/learnpython_intermediate.py
+++ Learning_Python/learnpython_intermediate.py
@@ -1,8 +1,20 @@
+def menyapaPelanggan(*names):
+    for name in names:
+        print ('Halo', name, 'Selamat Datang Ditoko Kami')
+        
+    return(names)
+
+menyapaPelanggan('Anna', 'Clara', 'Gabriel')
+
+names = input('Masukkan Nama Anda : ')
+
+menyapaPelanggan(names)
+
 def redmi(namaProduk, harga):
     print("Selamat Anda Telah Membeli")
     
-    print("Handphone yang anda beli : " + namaProduk)
-    print("Harga yang anda beli     : " + str(harga))
+    print("Handphone yang anda beli : " , namaProduk)
+    print("Harga yang anda beli     : " , str(harga))
     return(namaProduk, harga)
 
 namaProduk = input("Masukkan nama Handphone yang anda ingin beli : ")
