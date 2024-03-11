afb341390adef0bd0868b6935d5da5837e0a4bbf
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-11 07:10:15 +0700

    learning about global variable

--- Learning_Python/learnpython_intermediate.py
+++ Learning_Python/learnpython_intermediate.py
@@ -59,3 +59,13 @@ def outer_func():
   
 outer_func()
 
+
+
+def print_counter():
+  global counter
+  counter = 200
+  print('counter didalam fungsi =',counter) # nilai counter didalam fungsi
+
+counter = 100
+print_counter()
+print('counter diluar fungsi = ',counter) # nilai counter diluar fungsi
