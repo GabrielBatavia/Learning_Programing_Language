3ed7346b8e215686ed05732cd7145e7ac9178489
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-11 20:10:21 +0700

    learn about class-inheritence

---
+++ Learning_Python/class_inheritence.py
@@ -0,0 +1,13 @@
+class Person: # Parent class
+  nama = 'test'
+
+class Employee(Person): # child class
+  gaji = 1000
+  
+person1 = Person()
+employee1 = Employee()
+
+print('nama dari person', person1.nama)
+print('nama dari employee', employee1.nama)
+print('gaji dari employee',employee1.gaji)
+print('gaji dari person',person1.gaji)
