5e7138ac31844210a55869defe71cd29bd5f30b1
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-11 19:26:53 +0700

    learn make class and add mathod in class

--- Learning_Python/learnpython_intermediate.py
+++ Learning_Python/learnpython_intermediate.py
@@ -94,3 +94,12 @@ class Cat: # membuat class Cat
 
 nobi = Cat() # membuat sebuah instance dari Cat
 print(nobi)
+
+
+
+class Cat: # membuat class Cat
+  def meow(self): # fungsi meow didalam class Cat
+   print('meowwww') 
+
+nobi = Cat() # membuat sebuah instance dari Cat
+nobi.meow() # setelah membuat object dari Cat, kita bisa memanggil method meow 
