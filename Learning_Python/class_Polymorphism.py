a5a23e95b4ddb566d26551dd946cad6c37004465
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-14 20:43:11 +0700

    another example in Polymorphism

--- Learning_Python/class_Polymorphism.py
+++ Learning_Python/class_Polymorphism.py
@@ -26,3 +26,31 @@ obj_parr.flight()
  
 obj_peng.intro()
 obj_peng.flight()
+
+
+
+#make a loop in tuple
+
+class Hewan():
+    def suara(self):
+      print("Suara hewan")
+
+class Kucing(): 
+    def suara(self):
+       print("Meow")
+
+    def harapan_hidup(self): 
+      print("Harapan hidup kucing adalah 10 tahun.") 
+   
+    def warna_tubuh(self): 
+      print("Warna tubuh kucing pada umumnya adalah kuning") 
+   
+class Anjing(): 
+    def suara(self):
+      print("Woof")
+
+    def harapan_hidup(self): 
+      print("Harapan hidup anjing adalah 15 tahun.") 
+   
+    def warna_tubuh(self): 
+      print("Warna tubuh anjing pada umumnya adalah hitam") 
