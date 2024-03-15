f029523054d5650ac5a052fb1b3a065f63cb8b19
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-15 11:18:37 +0700

    use setter

--- Learning_Python/class_Encapsulation.py
+++ Learning_Python/class_Encapsulation.py
@@ -36,4 +36,34 @@ orang_obj.tampilkan()
 
 #mengakses langsung variable
 print(orang_obj.name)
-print(orang_obj.__age)
+#print(orang_obj.__age)
+
+
+
+
+#
+class Orang:
+  def __init__(self, name, age=0):
+    self.name = name
+    self.__age = age
+
+  def setAge(self, age):
+    self.__age = age
+ 
+  def tampilkan(self):
+    print(self.name)
+    print(self.__age)
+    print("----------")
+ 
+orang_obj = Orang('Budi', 30)
+
+#mengakses melalui metode
+orang_obj.tampilkan()
+
+#mengubah data tanpa setter tidak memungkinkan
+orang_obj.__age = 50
+orang_obj.tampilkan()
+
+#mengubah data harus memalui setter
+orang_obj.setAge(27)
+orang_obj.tampilkan()
