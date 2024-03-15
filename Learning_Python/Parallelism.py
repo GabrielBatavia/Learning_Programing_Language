72214def717424687dc5f551a056b01e99395a43
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-15 11:46:10 +0700

    complete the example

--- Learning_Python/Parallelism.py
+++ Learning_Python/Parallelism.py
@@ -18,3 +18,21 @@ def print_with_static_sleep(total_iteration):
     for x in range(total_iteration):
         print("Hello from static-sleep interval, pengulangan ke-{}".format(x))
         time.sleep(2) # fungsi akan berhenti selama 2 second
+        
+if __name__ == "__main__":
+	# creating thread
+	t1 = threading.Thread(target=print_with_dynamic_sleep, args=(5,))
+	t2 = threading.Thread(target=print_with_static_sleep, args=(5,))
+
+	# memulai thread pertama
+	t1.start()
+	# memulai thread kedua
+	t2.start()
+
+	# tunggu sampai thread 1 selesai dilaksanakan
+	t1.join()
+	# tunggu sampai thread 2 selesai dilaksanakan
+	t2.join()
+
+	# both threads completely executed
+	print("Selesai!")
