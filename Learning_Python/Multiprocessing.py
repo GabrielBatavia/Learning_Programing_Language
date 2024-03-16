740062168d688a5079011b548900581f128ed541
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-16 22:14:55 +0700

    modify the example

--- Learning_Python/Multiprocessing.py
+++ Learning_Python/Multiprocessing.py
@@ -9,3 +9,12 @@ def print_with_dynamic_sleep(total_iteration):
     for x in range(total_iteration):
         print("Hello from dynamic-sleep interval, pengulangan ke-{}".format(x))
         time.sleep(x) # fungsi akan berhenti selama x second, akan berubah2 secara dinamis
+        
+def print_with_static_sleep(total_iteration):
+    """
+	fungsi berikut akan melakukan print command
+	dengan interval yang statis/sama
+    """
+    for x in range(total_iteration):
+        print("Hello from static-sleep interval, pengulangan ke-{}".format(x))
+        time.sleep(2) # fungsi akan berhenti selama 2 second
