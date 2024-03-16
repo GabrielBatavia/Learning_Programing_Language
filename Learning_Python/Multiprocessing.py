4b19677ec21ff964a0a164a0178b57d19525a44d
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-16 22:14:22 +0700

    give example

--- Learning_Python/Multiprocessing.py
+++ Learning_Python/Multiprocessing.py
@@ -1,2 +1,11 @@
 import multiprocessing # import library multiprocessing
 import time # import library time
+
+def print_with_dynamic_sleep(total_iteration):
+    """
+    fungsi berikut akan melakukan print command
+    dengan interval yang berbeda-beda
+    """
+    for x in range(total_iteration):
+        print("Hello from dynamic-sleep interval, pengulangan ke-{}".format(x))
+        time.sleep(x) # fungsi akan berhenti selama x second, akan berubah2 secara dinamis
