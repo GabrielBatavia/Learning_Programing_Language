668db295a547081363205f51ec2ae6121f99b93c
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-16 22:15:23 +0700

    complete the example code

--- Learning_Python/Multiprocessing.py
+++ Learning_Python/Multiprocessing.py
@@ -18,3 +18,21 @@ def print_with_static_sleep(total_iteration):
     for x in range(total_iteration):
         print("Hello from static-sleep interval, pengulangan ke-{}".format(x))
         time.sleep(2) # fungsi akan berhenti selama 2 second
+
+if __name__ == "__main__":
+	# creating thread
+	p1 = multiprocessing.Process(target=print_with_dynamic_sleep, args=(5,))
+	p2 = multiprocessing.Process(target=print_with_static_sleep, args=(5,))
+
+	# memulai process pertama
+	p1.start()
+	# memulai process kedua
+	p2.start()
+
+	# tunggu sampai process 1 selesai dilaksanakan
+	p1.join()
+	# tunggu sampai process 2 selesai dilaksanakan
+	p2.join()
+
+	# both threads completely executed
+	print("Selesai!")
