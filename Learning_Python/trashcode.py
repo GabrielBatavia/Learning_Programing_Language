32fc392dda53f900bdd32c22db65657284a59931
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-20 22:20:13 +0700

    learn more about function

--- Learning_Python/trashcode.py
+++ Learning_Python/trashcode.py
@@ -21,3 +21,12 @@ namaProduk = input("Masukkan nama Handphone yang anda ingin beli : ")
 harga = float(input("Masukkan harga Handphone yang anda ingin dibeli   : "))
 
 redmi(namaProduk, harga)
+
+
+def laptop(namaProduk, harga):
+    print("Selamat Anda Telah Membeli")
+    
+    print("Laptop yang anda beli : " , namaProduk)
+    print("Harga yang anda beli     : " , str(harga))
+    return(namaProduk, harga)
+
