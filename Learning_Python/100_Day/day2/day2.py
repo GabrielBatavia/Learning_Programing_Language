a30fe8af2ba76db4154d41d9166d56d7a8dbab6a
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-10 04:09:51 +0700

    learn primitive datatype in python

---
+++ Learning_Python/100_Day/day2/day2.py
@@ -0,0 +1,28 @@
+# Data Types
+
+# String
+print("Hallo")
+print('Hallo'[0])
+print('Hallo'[-1])
+
+# Integer
+a = 10
+b = 15
+c = a + b
+print(c)
+
+print(123 + 345)
+
+print(123_456_678)
+
+# Float 
+
+d = 3.14159
+print(d)
+
+# Boolean
+True
+False
+
+print(True)
+print(False)
