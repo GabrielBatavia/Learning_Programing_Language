57d39ac7d4e4431dd8c74e485aebece1509539f1
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-10 04:32:43 +0700

    exercise1 that make program that will add 2 digit numbers from one input

---
+++ Learning_Python/100_Day/day2/exercise1.py
@@ -0,0 +1,8 @@
+# Make a program that adds the digit in a 2 digit number
+
+print("Input your 2 numbers")
+two_digit_number = input()
+print(type(two_digit_number))
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+print(first_digit + second_digit)
