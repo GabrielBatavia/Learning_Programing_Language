dccfcc037afb303b38f5c92b976eb2a8ee7f88a4
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-11 04:59:05 +0700

    start write code for exercise4 and make automatic pizza program

---
+++ Learning_Python/100_Day/day3/exercise4.py
@@ -0,0 +1,19 @@
+# buil automatic pizza order program
+
+print("Thankyou for choosing Python Pizza delivery")
+size = input("What suze pizza do you want? s, m, or l : ")
+
+add_pepperoni = input("Do you want pepperoni? y or n : ")
+extra_cheese = input("Do you want ectra cheese? y or n : ")
+
+# make the bill
+size_s = 15
+size_m = 20
+size_l = 25
+
+if size == s:
+    print(f"You order pizza size {size} and you bill get charge around {size_s}$")
+elif size == m:
+    print(f"You order pizza size {size} and you bill get charge around {size_m}$")
+else :
+    print(f"You order pizza size {size} and you bill get charge around {size_l}$")
