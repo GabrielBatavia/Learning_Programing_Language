9403e41decdd4c5aee5b576bb63d4c0fe05ba2b9
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-11 03:45:42 +0700

    learn about nested else if statment

--- Learning_Python/100_Day/day3/day3.py
+++ Learning_Python/100_Day/day3/day3.py
@@ -1,7 +1,15 @@
 print("Welcome to the rollercoaster!")
 height = int(input('What is your height in cm? '))
+age = int(input("Please enter your age : "))
 
 if height >= 120:
     print("You can ride the rollercoaster")
+    
+    if age < 12:
+        print("You have to pay 5$ for the ticket")
+    elif age <= 18:
+        print("You have to pay 7$ for the ticket")
+    else :
+        print("Please pay 12$ for the ticket")
 else :
     print("You can't ride the rollercoaster, please grow taller")
--- Learning_Python/100_Day/day3/exercise1.py
+++ Learning_Python/100_Day/day3/exercise1.py
@@ -1,14 +1,8 @@
 # make program to check odd or even
 
 number = int(input("Please enter your number : "))
-age = int(input("Please enter your age : "))
 
 if number % 2  == 0:
     print("This is an even number")
-    
-    if age >= 18:
-        print("You have to pay 18$ for the ticket")
-    else :
-        print("Please pay 7$ for the ticket")
 else :
     print("This is an odd number")
