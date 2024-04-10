18d73171d9788293c9c749b405527c1e7b2877b7
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-11 04:00:25 +0700

    fixed the if else statment condition in exercise2

--- Learning_Python/100_Day/day3/exercise2.py
+++ Learning_Python/100_Day/day3/exercise2.py
@@ -5,13 +5,13 @@ weight = int(input("Please enter your weight : "))
 
 bmi = weight / (height ** 2)
 
-if bmi <= 18.5 :
+if bmi < 18.5 :
     print(f"You bmi is {round(bmi, 2)}, you are underweight")
-elif bmi <= 25 :
+elif bmi < 25 :
     print(f"You bmi is {round(bmi, 2)}, you are normal")
-elif bmi <= 30 :
+elif bmi < 30 :
     print(f"You bmi is {round(bmi, 2)}, you are overweight")
-elif bmi <= 35 :
+elif bmi < 35 :
     print(f"You bmi is {round(bmi, 2)}, you are obese")
 else :
     print(f"you bmi is {round(bmi, 2)}, you are clinicaly obese")
