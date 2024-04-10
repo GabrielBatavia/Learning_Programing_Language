b0227a72aa209a81a35251172cb82a868fdd758e
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-11 05:11:23 +0700

    finished the pizza order program by my self

--- Learning_Python/100_Day/day3/exercise4.py
+++ Learning_Python/100_Day/day3/exercise4.py
@@ -11,9 +11,48 @@ size_s = 15
 size_m = 20
 size_l = 25
 
-if size == s:
+cheese = 1
+
+
+if size == "s":
     print(f"You order pizza size {size} and you bill get charge around {size_s}$")
-elif size == m:
+    pepperroni = 0
+    if add_pepperoni == "y":
+        pepperroni = 2
+        
+    if extra_cheese == "y":
+        total_bill = size_s + pepperroni + cheese
+        print(f"You add additional pepperoni and cheese, your total bill is {total_bill}$")
+    else :
+        total_bill = size_s + pepperroni
+        print(f"You add additional pepperoni, your total bill is {total_bill}$")
+
+            
+elif size == "m":
     print(f"You order pizza size {size} and you bill get charge around {size_m}$")
+    pepperroni = 0
+
+    if add_pepperoni == "y":
+        pepperroni = 3
+        
+    if extra_cheese == "y":
+        total_bill = size_m + pepperroni + cheese
+        print(f"You add additional pepperoni and cheese, your total bill is {total_bill}$")
+    else :
+        total_bill = size_m + pepperroni
+        print(f"You add additional pepperoni, your total bill is {total_bill}$")
+
 else :
     print(f"You order pizza size {size} and you bill get charge around {size_l}$")
+    pepperroni = 0
+
+    if add_pepperoni == "y":
+        pepperroni = 3
+        
+    if extra_cheese == "y":
+        total_bill = size_l + pepperroni + cheese
+        print(f"You add additional pepperoni and cheese, your total bill is {total_bill}$")
+    else :
+        total_bill = size_l + pepperroni
+        print(f"You add additional pepperoni, your total bill is {total_bill}$")
+
