a56f9608e414c187ada0d47d1d1da661f852c2ca
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-11 03:28:59 +0700

    do exercise 1 in day 3 and make program to check odd or even

---
+++ Learning_Python/100_Day/day3/exercise1.py
@@ -0,0 +1,8 @@
+# make program to check odd or even
+
+number = int(input("Please enter your number : "))
+
+if number % 2  == 0:
+    print("This is an odd number")
+else :
+    print("This is an even number")
