2309bf45aa292d3ddfc2a49fe72038453054c52d
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-11 06:59:55 +0700

    start build the project and its name Get_Your_Oshi game!

---
+++ Learning_Python/100_Day/day3/Get_Your_Oshi.py
@@ -0,0 +1,37 @@
+start = input("Wana start the game? y or n : ")
+
+
+if start == "y":
+    
+    print('''
+        /:""|       .@@@@@,
+        |:`66|_      @@@@@@@@,
+        C`    _)     aa`@@@@@@
+        \ ._|      (_   ?@@@@
+        ) /        =' @@@@"
+        /`\\         \(```
+        || |Y|       //`|
+        || |.|      / | ||
+        || |.|      \ | ||
+        || |.|       \| ||
+        :| |=:        |_||
+        ||_|,|        |_| |
+        \)))||        (((  |
+        |  ||        |____|
+        |  ||        |____|
+        >  ))         | ||
+        | ||          | ||
+        | ||          | ||
+        |_||__        /~))
+        (____))      /_/YY
+        ''')
+    
+    print("=====The story start======")
+    name = input("Enter your character name : ")
+    print("================================")
+    print(f"Your {name} and now your in fx sudirman...")
+    print(f"How lucky your!!! you win the roulate and get 2shoot with Ella!!!")
+    
+
+else:
+    print("Bye, see you next time")
