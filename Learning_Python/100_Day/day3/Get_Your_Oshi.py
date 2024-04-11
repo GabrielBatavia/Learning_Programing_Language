f6247856159e755e4c2192cd3b436a4ec3414845
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-11 07:08:46 +0700

    continue write the story for the game

--- Learning_Python/100_Day/day3/Get_Your_Oshi.py
+++ Learning_Python/100_Day/day3/Get_Your_Oshi.py
@@ -2,6 +2,13 @@ start = input("Wana start the game? y or n : ")
 
 
 if start == "y":
+
+    print("=====The story start======")
+    name = input("Enter your character name : ")
+    print("================================")
+    print(f"Your {name} and now your in fx sudirman...")
+    print(f"How lucky your!!! you win the roulate and get 2shoot with Ella!!!")
+    print("================================")
     
     print('''
         /:""|       .@@@@@,
@@ -26,11 +33,25 @@ if start == "y":
         (____))      /_/YY
         ''')
     
-    print("=====The story start======")
-    name = input("Enter your character name : ")
     print("================================")
-    print(f"Your {name} and now your in fx sudirman...")
-    print(f"How lucky your!!! you win the roulate and get 2shoot with Ella!!!")
+    print("After get the photo with your oshi...you so happy")
+    print("Now you want to get out from theater and looking for cheap food in fx")
+    print("================================")
+    print("But hey look over there!!!")
+    print("Ella walk to you and you two get really close")
+    print("================================")
+    
+    print("Ella stand in front of you but ella dosent say anything")
+    print("Your man! you have to make first move")
+    print("You wana say something : ")
+    print("1. Ella...what are you looking for?")
+    print("2. Go Away! im sacred!")
+    print("3. Uhmmm..excuse me....")
+    answer1 = input("Now what you gone say to Ella : ")
+    
+    
+    
+    
     
 
 else:
