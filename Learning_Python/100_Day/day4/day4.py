ea0568782f8312906587f73fc6de923babca71a9
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-12 05:49:53 +0700

    random floating point numbers

--- Learning_Python/100_Day/day4/day4.py
+++ Learning_Python/100_Day/day4/day4.py
@@ -7,3 +7,10 @@ print(random_integer)
 #import our module
 import myModule
 print(myModule.pi)
+
+# random floating point numbers
+random_float = random.random()
+print(random_float)
+
+random_float *= random_integer 
+print(random_float)
