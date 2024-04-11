9c81730eaf2a19d797eb44e6cb47514dee287b88
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-11 07:27:16 +0700

    finish the project and its really intresting game

--- Learning_Python/100_Day/day3/Get_Your_Oshi.py
+++ Learning_Python/100_Day/day3/Get_Your_Oshi.py
@@ -47,10 +47,104 @@ if start == "y":
     print("1. Ella...what are you looking for?")
     print("2. Go Away! im sacred!")
     print("3. Uhmmm..excuse me....")
-    answer1 = input("Now what you gone say to Ella : ")
+    answer1 = input("Now what you gone say to Ella? 1, 2, or 3? : ")
     
+    if answer1 == "1":
+        print("================================================================")
+        print(f"Ella : uhmmm...your {name} right? i just...you know...")
+        print("================================================================")
+        print("You looks Ella saying this with shy tones and now you feels like wana die cause you very happy")
+        print(f"What you should do {name}? You have to make next moves!")
+        print("1. Hold Ella hand")
+        print("2. Take a picture with Ella")
+        print("3. Ask Ella for dinner together in fx")
+        answer2 = input("Now what you should do? 1, 2, or 3? : ")
+        
+        if answer2 == "3":
+            print("================================================================")
+            print("You know what? Ella blush and she nod slowly...")
+            print("Ella : Yes...")
+            print("================================================================")
+            print("Now Your become the lucky person in this place...and you get happy Ending")
+            
+            print('''
+                  /:""|  .@@@@@,
+            (\/)  |:`66|_ @@@@@@@@,
+             \/   C`    _)aa`@@@@@@
+                  \ ._| (_   ?@@@@
+                  ) /   =' @@@@"
+                 /`\\    \(```
+                || |Y|  //`|
+                || |.| / | || (\/)
+                || |.| \ | ||  \/
+                || |.|  \| ||
+                :| |=:   |_||
+                ||_|,|   |_| |
+                \)))||   (((  |
+           (\/)  |  ||   |____|
+            \/   |  ||   |____|
+                 >  ))    | ||
+                 | ||     | ||
+                 | ||     | ||
+                 |_||__   /~))
+                 (____)) /_/YY
+
+                  ''')
+            
+        else :
+            print(f"WTF {name}!!! Your so disgusting...Thats very bad move from you! Go away!!!")
+            
+            
+    elif answer1 == "2":
+        print("================================================================")
+        print("Wtf man...After you saying that, Ella looks mad and runs out...Game Over!")
+        
+        
+    else:
+        print("================================================================")
+        print("================================================================")
+        print(f"Ella : uhmmm...your {name} right? i just...you know...")
+        print("================================================================")
+        print("You looks Ella saying this with shy tones and now you feels like wana die cause you very happy")
+        print(f"What you should do {name}? You have to make next moves!")
+        print("1. Hold Ella hand")
+        print("2. Take a picture with Ella")
+        print("3. Ask Ella for dinner together in fx")
+        answer2 = input("Now what you should do? 1, 2, or 3? : ")
+        
+        if answer2 == "3":
+            print("================================================================")
+            print("You know what? Ella blush and she nod slowly...")
+            print("Ella : Yes...")
+            print("================================================================")
+            print("Now Your become the lucky person in this place...and you get happy Ending")
     
-    
+            print('''
+                  /:""|  .@@@@@,
+            (\/)  |:`66|_ @@@@@@@@,
+             \/   C`    _)aa`@@@@@@
+                  \ ._| (_   ?@@@@
+                  ) /   =' @@@@"
+                 /`\\    \(```
+                || |Y|  //`|
+                || |.| / | || (\/)
+                || |.| \ | ||  \/
+                || |.|  \| ||
+                :| |=:   |_||
+                ||_|,|   |_| |
+                \)))||   (((  |
+           (\/)  |  ||   |____|
+            \/   |  ||   |____|
+                 >  ))    | ||
+                 | ||     | ||
+                 | ||     | ||
+                 |_||__   /~))
+                 (____)) /_/YY
+
+                  ''')
+            
+        else :
+            print(f"WTF {name}!!! Your so disgusting...Thats very bad move from you! Go away!!!")
     
     
 
