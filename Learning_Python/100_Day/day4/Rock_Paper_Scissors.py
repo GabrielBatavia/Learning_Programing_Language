71ed20c513a3ffaea2679e026367ca756a461ccf
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-12 19:47:05 +0700

    complete the code in Rock_Paper_Scissors with more complex

--- Learning_Python/100_Day/day4/Rock_Paper_Scissors.py
+++ Learning_Python/100_Day/day4/Rock_Paper_Scissors.py
@@ -5,9 +5,89 @@ print("Welcome to the Rock Paper Scissors Program")
 
 user_guess = int(input("Please enter you guess: 1.Rock, 2.Paper, 3.Scissors: "))
 
-program_aswer = random.randint(1, 3) 
+if user_guess == 1:
+    print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
 
-if user_guess == program_aswer:
-    print("You Win")
+elif user_guess == 2:
+    print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+
+elif user_guess == 3:
+    print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+    
 else :
-    print("You losses")
+    print("Please enter correct guess number")
+    exit()
+
+program_answer = random.randint(1, 3)
+
+if program_answer == 1:
+    print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+
+elif program_answer == 2:
+    print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+
+else :
+    print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+
+if user_guess == 1 and program_answer == 3:
+    print("You Win")
+elif user_guess == 1 and program_answer == 2:
+    print("You lose")
+elif user_guess == 1 and program_answer == 1:
+    print("You tie")
+
+if user_guess == 2 and program_answer == 1:
+    print("You win")
+elif user_guess == 2 and program_answer == 3:
+    print("You lose")
+elif user_guess == 2 and program_answer == 2:
+    print("You tie")
+
+if user_guess == 3 and program_answer == 2:
+    print("You win")
+elif user_guess == 3 and program_answer == 1:
+    print("You lose")
+elif user_guess == 3 and program_answer == 3:
+    print("You tie")
