c6f8cf4c47da1325815b21d497bbbd6c037c7097
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-13 00:51:38 +0700

    finish the code by my self and get the calculate average height program

--- Learning_Python/100_Day/day5/exercise1.py
+++ Learning_Python/100_Day/day5/exercise1.py
@@ -1,7 +1,25 @@
 # program that caculates avarange studeng height from list
 
 # Input a pyrhon list of student heights
-student_heights = input().split()
+student_heights = input("Input all your student height : ").split()
+
+total_height = 0
+total_height = int(total_height)
+
+number_of_students = 0
+number_of_students = int(number_of_students)
+
+averages_height = 0
+averages_height = int(averages_height)
 
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
+    total_height += student_heights[n]
+    number_of_students += 1
+    averages_height += student_heights[n]
+
+averages_height /= number_of_students
+
+print(f"total height : {total_height}")
+print(f"number of students : {number_of_students}")
+print(f"Average height : {averages_height}")
