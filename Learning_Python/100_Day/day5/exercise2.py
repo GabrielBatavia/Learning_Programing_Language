55ee42e8a954d1be2253c96dd32b9057defd1d0f
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-13 01:33:01 +0700

    finish exercise 2 by my self

--- Learning_Python/100_Day/day5/exercise2.py
+++ Learning_Python/100_Day/day5/exercise2.py
@@ -1,9 +1,14 @@
 # Make program that calculate the highest number on list
+# not allowed use max min function
 
 # Input a list of student scores
-student_scores = input().split()
+student_scores = input("Input all your student score : ").split()
+highest_score = 0 
 
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
+     
+    if student_scores[n] > highest_score:
+        highest_score = student_scores[n]
     
-highest_score = 0 
+print(f"The highest score in class : {highest_score}")
