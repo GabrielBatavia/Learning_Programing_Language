f3258c9688064868fa8ab157d5115436fa011220
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-15 12:07:52 +0700

    start day7 and make first step to the hangman.py

---
+++ Learning_Python/100_Day/Hangman.py
@@ -0,0 +1,9 @@
+# Step 1
+import random
+
+word_list = ["ardvark", "baboon", "camel"]
+
+# Todo-1 - Rnadomly choose a word from the word_list and assign it to a variab;e called chosen_word
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
