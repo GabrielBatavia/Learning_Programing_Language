08f2812ecf0275fa187016ab4deec07d23795c3b
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-15 12:35:21 +0700

    add code for choose randomly the guess word and check letter by letter to the choosen letter from user

--- Learning_Python/100_Day/Hangman.py
+++ Learning_Python/100_Day/Hangman.py
@@ -4,6 +4,16 @@ import random
 word_list = ["ardvark", "baboon", "camel"]
 
 # Todo-1 - Rnadomly choose a word from the word_list and assign it to a variab;e called chosen_word
-
 chosen_word = random.choice(word_list)
+chosen_list = list(chosen_word)
 print(chosen_word)
+print(chosen_list)
+
+# Todo-2 Ask the user to guess a letter and assign their answer to a variable called guess. make guess lowercase
+user_choose = input("Guess a letter : ")
+user_choose = user_choose.lower()
+
+# Todo-3 Check if the letter is the user guessed (guess) is one of the leters in the chosen_word
+for n in range(0, len(chosen_list)):
+    if user_choose == chosen_list[n]:
+        print(chosen_list[n])
