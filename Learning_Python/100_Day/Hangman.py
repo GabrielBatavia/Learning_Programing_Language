be0955d263496a1aa9e6197b37a378072a088956
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-15 12:54:47 +0700

    add the display for empety list in our chosen_list

--- Learning_Python/100_Day/Hangman.py
+++ Learning_Python/100_Day/Hangman.py
@@ -1,19 +1,25 @@
-# Step 1
+# Step 2
 import random
 
 word_list = ["ardvark", "baboon", "camel"]
 
-# Todo-1 - Rnadomly choose a word from the word_list and assign it to a variab;e called chosen_word
 chosen_word = random.choice(word_list)
 chosen_list = list(chosen_word)
-print(chosen_word)
-print(chosen_list)
 
-# Todo-2 Ask the user to guess a letter and assign their answer to a variable called guess. make guess lowercase
+# Testing code
+print(f"Pssst, the solution is {chosen_word}")
+
+# Create an empety list called display
+for n in range(0, len(chosen_list)):
+    print("_ ", end="", flush=True)
+
+
+print("")
 user_choose = input("Guess a letter : ")
 user_choose = user_choose.lower()
 
-# Todo-3 Check if the letter is the user guessed (guess) is one of the leters in the chosen_word
 for n in range(0, len(chosen_list)):
     if user_choose == chosen_list[n]:
         print(chosen_list[n])
+
+
