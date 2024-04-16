446098ad71a088394609e005b9dc54affc919fd7
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-16 08:39:12 +0700

    function with one and more parameters and keyword arguments

--- Learning_Python/100_Day/day8/day8.py
+++ Learning_Python/100_Day/day8/day8.py
@@ -1,3 +1,5 @@
+# Make a function
+
 def great(a, b):
     print("This your total amount")
     c = a + b 
@@ -5,3 +7,23 @@ def great(a, b):
     return c
 
 great(10, 1000)
+# print(c) #its dont work cause c is only in function
+
+# function with input parameters
+def greeting(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+names = input("Whats your name? : ")
+greeting(names)
+
+# function with more than one argument
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"I meet you at {location}")
+
+loc = input("Please enter your location : ")
+greet_with(names, loc)
+
+# Keyword arguments
+greet_with(name="Gabriel", location="Jakbar")
