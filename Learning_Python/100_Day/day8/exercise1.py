b4d20aeec50dbfbddc9944087e4eb576cdf9a7b8
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-16 08:52:30 +0700

    start exercise1 day 8 and write the base code

---
+++ Learning_Python/100_Day/day8/exercise1.py
@@ -0,0 +1,17 @@
+# You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
+# number of cans = (wall height x wall width) ÷ coverage per can.
+# IMPORTANT: Notice the name of the function and parameters must match those on line 13 for the code to work.
+
+# Write your code below this line 👇
+
+
+
+
+# Write your code above this line 👆
+# Define a function called paint_calc() so the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input()) # Height of wall (m)
+test_w = int(input()) # Width of wall (m)
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
