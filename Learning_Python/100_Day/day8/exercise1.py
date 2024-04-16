add664bad87bc58864411ef7fbcd87cb7f890835
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-16 12:23:37 +0700

    write and finish exercise1 (make number of cans calculation)

--- Learning_Python/100_Day/day8/exercise1.py
+++ Learning_Python/100_Day/day8/exercise1.py
@@ -2,16 +2,20 @@
 # number of cans = (wall height x wall width) ÷ coverage per can.
 # IMPORTANT: Notice the name of the function and parameters must match those on line 13 for the code to work.
 
-# Write your code below this line 👇
+# Write your code below this line 
+import math
 
-
-
-
-# Write your code above this line 👆
+def paint_calc(height, width, cover):
+    number_cans = (height * width) / cover
+    round_up_cans = math.ceil(number_cans)
+    print(f"You'll need {round_up_cans} cans of point")
+    
+# Write your code above this line 
 # Define a function called paint_calc() so the code below works.   
 
-# 🚨 Don't change the code below 👇
-test_h = int(input()) # Height of wall (m)
-test_w = int(input()) # Width of wall (m)
+# Don't change the code below 
+test_h = int(input("Input your height of wall : ")) # Height of wall (m)
+test_w = int(input("Input your width of wall : ")) # Width of wall (m)
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
+
