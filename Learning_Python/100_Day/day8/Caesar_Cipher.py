e8bcc01b4f5c818de4e7f71de3b82d45c5abeb02
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-16 18:45:26 +0700

    modify the function with better logic (previosly we have trouble with indexing from our list)

--- Learning_Python/100_Day/day8/Caesar_Cipher.py
+++ Learning_Python/100_Day/day8/Caesar_Cipher.py
@@ -1,28 +1,21 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-
+# The encryption function
 def encrypt (input_text, shift_number):
     cipher_text = []
     for letter in input_text:
         index_alphabet = alphabet.index(letter)
-        cipher_letter = index_alphabet * shift_number
+        cipher_letter = index_alphabet + shift_number
+        
         cipher_text += alphabet[cipher_letter]
-    
-    print(f"{' '.join(cipher_text)}")
         
+    print(f"{''.join(cipher_text)}")
         
+# Call the encrypt function
 encrypt(text, shift)
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
