d5cfcc144e4af1227509e723dadf8c78f4206d07
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-16 19:53:24 +0700

    make while loop so user can restart program and use that again

--- Learning_Python/100_Day/day8/Caesar_Cipher.py
+++ Learning_Python/100_Day/day8/Caesar_Cipher.py
@@ -4,13 +4,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 from caesar_art import logo
 
-# User Input
 print(logo)
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 
 """
 # The encryption function
@@ -35,33 +29,44 @@ def decrypt(input_text, shift_number):
         
     print(f"The decoded text is {''.join(decoded_text)}")
 """
+restart_program = True
 
-# Join the encrypt and decrypt funtion together
-def ceaser(input_text, shift_number, the_direction):
-    ceaser_text = []
-    for char in input_text:
-        if char in alphabet:
-            position = alphabet.index(char)
-            if the_direction == "encode":
-                ceaser_index = position + shift_number
-            elif the_direction == "decode":
-                ceaser_index = position - shift_number
+while not restart_program == False:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    # Join the encrypt and decrypt funtion together
+    def ceaser(input_text, shift_number, the_direction):
+        ceaser_text = []
+        for char in input_text:
+            if char in alphabet:
+                position = alphabet.index(char)
+                if the_direction == "encode":
+                    ceaser_index = position + shift_number
+                elif the_direction == "decode":
+                    ceaser_index = position - shift_number
+                
+                ceaser_text += alphabet[ceaser_index]
+            else:
+                ceaser_text += char
             
-            ceaser_text += alphabet[ceaser_index]
-        else:
-            ceaser_text += char
-        
-    print(f"The {the_direction} text is {''.join(ceaser_text)}")
+        print(f"The {the_direction} text is {''.join(ceaser_text)}")
 
 
-# What if the user input shit greater than the len aplphabet?
-shift = shift % 26
-print(shift)
+    # What if the user input shit greater than the len aplphabet?
+    shift = shift % 26
+    print(shift)
 
-# Call the ceaser function
-ceaser(text, shift, direction)
-
-
+    # Call the ceaser function
+    ceaser(text, shift, direction)
+    
+    input_restart = input("Are you want to restart the program? yes or no? : ")
+    input_restart = input_restart.lower()
+    
+    if input_restart == "no":
+        restart_program = False
 
 
 
