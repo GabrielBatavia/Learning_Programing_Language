38e18a700b2a684715d74995e971d909b57a3f3b
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-17 21:06:33 +0700

    add the code by my own code and make the loop for the program

--- Learning_Python/100_Day/day9/The_Secret_Auction.py
+++ Learning_Python/100_Day/day9/The_Secret_Auction.py
@@ -11,12 +11,24 @@ auction_dict = {}
 # make function to add to the dictionary
 def add_to_dict(name, bid_value):
     auction_dict[name] = bid_value 
+
+continue_program = True
+
+while not continue_program == False:
     
-# get username
-user_name = input("Enter your username : ")
-# get the bid
-user_bid = int(input("Enter your bid : $"))
+    # get username
+    user_name = input("Enter your username : ")
+    # get the bid
+    user_bid = int(input("Enter your bid : $"))
+    # Add to the dictionary
+    add_to_dict(user_name, user_bid)
 
-add_to_dict(user_name, user_bid)
+    user_choice = input("Are you want to continue the program? yes or no : ")
+    if user_choice == "no":
+        continue_program = False
 
+for bid in auction_dict:
+    max_bid = auction_dict[bid]
+    
+        
 print(auction_dict)
