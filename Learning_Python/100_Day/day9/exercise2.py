2040b616e899f85f30da49b61abcdb6a99a2cf08
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-17 20:42:32 +0700

    completed the log_travel program and finish exercise 2 in day 9

--- Learning_Python/100_Day/day9/exercise2.py
+++ Learning_Python/100_Day/day9/exercise2.py
@@ -4,9 +4,9 @@
 # Your job is to create a function that can add new countries to this list.
 
 
-country = input() # Add country name
-visits = int(input()) # Number of visits
-list_of_cities = eval(input()) # create list from formatted string
+country = input("Please input the country : ") # Add country name
+visits = int(input("Input how many times you go there : ")) # Number of visits
+list_of_cities = eval(input("Make a list in that country that allready you visits : Make like ['bekasi', 'bogor'] : ")) # create list from formatted string
 
 travel_log = [
   {
@@ -22,9 +22,16 @@ travel_log = [
 ]
 # Do NOT change the code above 
 
+# make the function to add to list
 def add_new_country(new_country, num_visit, city_list):
     new_dict_travel = {}
-    new_dict_travel.append(travel_log)
     new_dict_travel["country"] = new_country
     new_dict_travel["visits"] = num_visit
     new_dict_travel["cities"] = city_list
+    travel_log.append(new_dict_travel)
+
+
+# Do not change the code below 
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
