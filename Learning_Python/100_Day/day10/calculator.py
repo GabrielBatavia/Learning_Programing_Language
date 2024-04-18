9c8bb9f44c8b953f5f67f9226492cc0547fa2b2b
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-18 20:43:23 +0700

    make the user input for arguments and call the function by choosen function by user

--- Learning_Python/100_Day/day10/calculator.py
+++ Learning_Python/100_Day/day10/calculator.py
@@ -15,3 +15,23 @@ def multiply(n1, n2):
 # divide function
 def divide(n1, n2):
     return n1 / n2
+
+# make dictionary for calculations
+math_operator = {"+": add,
+                 "-": subtrack,
+                 "*": multiply,
+                 "/": divide,}
+
+# get input from user for our argument
+num1 = int(input("Whats the first number? : "))
+num2 = int(input("Whats the second number? : "))
+
+for key in math_operator:
+    print(key)
+
+operation_symbol = input("Pick an operation from the line above: ")
+
+# get choosen operation function and call it 
+choosen_function = math_operator[operation_symbol]
+result = choosen_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {result}")
