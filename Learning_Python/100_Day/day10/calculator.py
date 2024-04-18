42e80fca03e54bad1e8f709b4c69e9baf0ef2570
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-18 20:07:01 +0700

    start create calculator program and add calculation function for adding, subtract, multiply and divide

---
+++ Learning_Python/100_Day/day10/calculator.py
@@ -0,0 +1,17 @@
+# make calculator program
+
+# add function 
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract function
+def subtrack(n1, n2):
+    return n1 - n2
+
+# multiply function
+def multiply(n1, n2):
+    return n1 * n2
+
+# divide function
+def divide(n1, n2):
+    return n1 / n2
