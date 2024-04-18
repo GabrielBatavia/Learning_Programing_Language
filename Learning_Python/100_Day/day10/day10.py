a327dfa50af48250ef854e61c216b585028aaa75
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-18 12:08:07 +0700

    get better concept of return and make multiple return values

--- Learning_Python/100_Day/day10/day10.py
+++ Learning_Python/100_Day/day10/day10.py
@@ -9,8 +9,11 @@ first_name = input("Inpur your first name : ")
 last_name = input("Input your last name : ")
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didnt input your name"
     f_name = f_name.title()
     l_name = l_name.title()
     return f"{f_name} {l_name}"
+    # we cant add something after return, cause return is end of function
 
 print(format_name(first_name, last_name))
