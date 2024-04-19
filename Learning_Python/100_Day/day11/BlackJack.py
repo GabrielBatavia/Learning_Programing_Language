52284e8f954da840844d9310d65443b03b620ac5
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-19 11:37:32 +0700

    simplify our choosen card code by using function and cut all of repetitive code

--- Learning_Python/100_Day/day11/BlackJack.py
+++ Learning_Python/100_Day/day11/BlackJack.py
@@ -1,34 +1,22 @@
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
+############### Blackjack House #####################
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 import random
 
-user_first_card = random.choice(cards)
-user_second_card = random.choice(cards)
-computer_first_card = random.choice(cards)
-computer_second_card = random.choice(cards)
+def choose_card():
+    card = random.choice(cards)
+    return card
 
 user_cards_list = []
-user_cards_list.append(user_first_card)
-user_cards_list.append(user_second_card)
-user_total = user_first_card + user_second_card
-
 computer_cards_list = []
-computer_cards_list.append(computer_first_card)
+
+for i in range(2):
+    user_cards_list.append(choose_card())
+    computer_cards_list.append(choose_card())
 
 print(f"Your card are {user_cards_list} ")
-print(f"Computer cards are {computer_cards_list}")
+print(f"Computer first cards are [{computer_cards_list[0]}]")
 
 get_another_card = True
 
