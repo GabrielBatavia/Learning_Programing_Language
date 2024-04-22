7dbd876c7e5b655156a061edd035e2e2bfd44763
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-22 08:45:14 +0700

    done with exercise1

--- Learning_Python/100_Day/day13/exercise1.py
+++ Learning_Python/100_Day/day13/exercise1.py
@@ -1,9 +1,10 @@
 # Modify the code to fix the program.
 
-number = int(input()) # Which number do you want to check?
+number = int(input("Which number do you want to check? : ")) # Which number do you want to check?
 
-if number % 2 = 0:
+if number % 2 == 0:
   print("This is an even number.")
 else:
   print("This is an odd number.")
   
+## Error in line 5 cuase if number % 2 = 0
