482d28de5853b5b98b2407f1afbdf4f5ea2c0cef
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-22 21:04:42 +0700

    add logic funtion to compare the following count

--- Learning_Python/100_Day/day14/higher_lower_followers.py
+++ Learning_Python/100_Day/day14/higher_lower_followers.py
@@ -4,7 +4,14 @@ import random
 account = {}
 compare_followers = 0
 
+# All function
+
 def get_compare():
+    """This function will make random accounts for comparison
+
+    Returns:
+        _dict_: _the account of the famous person_
+    """
     global account
     account = random.choice(data)
     return account
@@ -17,6 +24,15 @@ def account_name_print(first_account, second_account):
     
     print(f"The first account is {account_name1}, {account_desc1}")
     print(f"The first account is {account_name2}, {account_desc2}")
+    
+def compare_followers(compare, guess):
+    accountcompare_followers = compare["follower_count"]
+    accountguess_followers = guess["follower_count"]
+    
+    if accountcompare_followers > accountguess_followers:
+        print("Your wrong")
+    else:
+        print("Your correct")
 
 
 # Main
@@ -28,3 +44,13 @@ if account1 == account2:
 
 account_name_print(account1, account2)
 
+user_guess = input("Which one have more following accounts? 1 or 2? : ")
+
+if user_guess == "1":
+    user_account = account1
+    compare_account = account2
+else:
+    compare_account = account1
+    user_account = account2
+
+compare_followers(compare_account, user_account)
