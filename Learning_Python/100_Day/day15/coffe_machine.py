ca2b26aa50a68e9ecc146c7a5e745782aa8ebebb
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-23 08:11:16 +0700

    add insert money funtion

--- Learning_Python/100_Day/day15/coffe_machine.py
+++ Learning_Python/100_Day/day15/coffe_machine.py
@@ -56,7 +56,22 @@ def report_resources():
         print(f"milk : {milk}")
         print(f"coffee : {coffee}")
 
+def insert_money():
+    print("Please insert coins")
+    quarters = int(input("How many quarters? : "))
+    dimes = int(input("How many dimes? : "))
+    nickels = int(input("How many nickels? : "))
+    penny = int(input("How many penny? : "))
+    
+    quarters_amount = quarters * 0.25
+    dimes_amount = dimes * 0.10
+    nickels_amount = nickels * 0.05
+    penny_amount = penny * 0.01
+    dolars = quarters_amount + dimes_amount + nickels_amount + penny_amount
+    print(f"Your total money : ${dolars}")
+
 # Main
 print_menu()
 user_order = input("What you want to order? espresso? latte? or cappuccino? : ")
 report_resources()
+insert_money()
