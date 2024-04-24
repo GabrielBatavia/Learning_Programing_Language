497de17353e42a8538de705dcbcc85eed6c7b723
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-24 07:52:59 +0700

    learn hor to add python packages and use PyPi

--- Learning_Python/100_Day/day16/day16.py
+++ Learning_Python/100_Day/day16/day16.py
@@ -1,17 +1,25 @@
 # oop
-import another_module
-print(another_module.another_variable)
+#import another_module
+#print(another_module.another_variable)
 
-from turtle import Turtle, Screen
+#from turtle import Turtle, Screen
 # make object
-tono = Turtle()
-print(tono)
-tono.shape("turtle")
-tono.color("green", "blue")
-tono.forward(200)
+#tono = Turtle()
+#print(tono)
+#tono.shape("turtle")
+#tono.color("green", "blue")
+#tono.forward(200)
 
 # screen is object
-my_screen = Screen()
-print(my_screen.canvheight) # canvheight is atribut
+#my_screen = Screen()
+#print(my_screen.canvheight) # canvheight is atribut
 
-my_screen.exitonclick() # exitonclick is method
+#my_screen.exitonclick() # exitonclick is method
+
+
+# try to use package that allready exists in python comunity
+# we should install the pacakge in python extensions
+
+import prettytable
+
+
