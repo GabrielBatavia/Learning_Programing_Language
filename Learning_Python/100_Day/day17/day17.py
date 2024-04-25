a43cfa2745f35c92fc4527a27010f36e46d50f88
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-25 07:51:12 +0700

    working with attributes, class constructors and the __init__() Function

--- Learning_Python/100_Day/day17/day17.py
+++ Learning_Python/100_Day/day17/day17.py
@@ -1,4 +1,23 @@
-class User:
+# primitive way to create class
+class User: # make the class
     pass    
 
-user_1 = User()
+user_1 = User() # make object
+user_1.id = "001" # adding atribute
+user_1.username = "Angela"
+
+print(user_1.username)
+
+
+# make more nice way
+class Person:
+    
+    def __init__(self, person_id, username):
+        print("new person is created...")
+        self.id = person_id
+        self.username = username
+        self.followers = 0 # we can setting default value so its not always have to in parameters
+
+person1 = Person("001", "Shalom")
+print(person1.id)
+print(person1.username)
