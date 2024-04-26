19af69a51dac24432e2f98710cb903912b263901
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-26 17:52:39 +0700

    playing around with tono the turtle using the turtle package

---
+++ Learning_Python/100_Day/day18/day18.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+tono_the_turtle = Turtle()
+tono_the_turtle.shape("turtle")
+tono_the_turtle.color("green", "yellow")
+tono_the_turtle.forward(100)
+tono_the_turtle.backward(300)
+tono_the_turtle.left(90)
+tono_the_turtle.forward(100)
+tono_the_turtle.right(90)
+tono_the_turtle.forward(100)
+
+screen = Screen()
+screen.exitonclick()
