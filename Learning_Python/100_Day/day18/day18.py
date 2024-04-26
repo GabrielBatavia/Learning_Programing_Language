45777d5178b39b576dca9f090503355aa04b9273
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-26 17:56:30 +0700

    do little challenge by make a square with tono

--- Learning_Python/100_Day/day18/day18.py
+++ Learning_Python/100_Day/day18/day18.py
@@ -3,12 +3,16 @@ from turtle import Turtle, Screen
 tono_the_turtle = Turtle()
 tono_the_turtle.shape("turtle")
 tono_the_turtle.color("green", "yellow")
+
+# challenge 1 ## make a square
 tono_the_turtle.forward(100)
-tono_the_turtle.backward(300)
 tono_the_turtle.left(90)
 tono_the_turtle.forward(100)
-tono_the_turtle.right(90)
+tono_the_turtle.left(90)
 tono_the_turtle.forward(100)
+tono_the_turtle.left(90)
+tono_the_turtle.forward(100)
+tono_the_turtle.left(90)
 
 screen = Screen()
 screen.exitonclick()
