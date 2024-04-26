03c998bf01bdde09690f93151fd1ac5ebb1b896d
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-26 18:59:30 +0700

    do challenge 3 and add make triangle, pentagon, and hexagon function

--- Learning_Python/100_Day/day18/day18.py
+++ Learning_Python/100_Day/day18/day18.py
@@ -17,8 +17,33 @@ def make_dash_line():
         tono.pd()
         tono.forward(10)
         tono.pu()
-    
-make_dash_line()
+#make_dash_line()
+
+# challenge 3
+# draw a triangle, square, pentagon, hexagon, heptagon, octagon
+# nonagon, and decagon
+# each shapes have random color and each side is 100 len
+
+def make_triangle():
+    for i in range(3):
+        tono.forward(100)
+        tono.left(120)
+        
+def make_pentagon():
+    for i in range(5):
+        tono.forward(100)
+        tono.left(72)
+
+def make_hexagon():
+    for i in range(6):
+        tono.forward(100)
+        tono.left(60)
+
+make_triangle()
+make_square()
+make_pentagon()
+make_hexagon()
+
 
 screen = Screen()
 screen.exitonclick()
