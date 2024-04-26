706d97b5578045e000de0e311a5b857a3b073167
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-26 20:33:15 +0700

    make random walk function and passed the challenge 4 by my own code, and hey its works!

--- Learning_Python/100_Day/day18/day18.py
+++ Learning_Python/100_Day/day18/day18.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 tono = Turtle()
 tono.shape("turtle")
@@ -81,8 +82,35 @@ def make_the_shapes():
     make_nonagon()
     make_decagon()
 
-make_the_shapes()
-wipe()
+#make_the_shapes()
+#wipe()
+
+
+# Challenge 4
+# Draw a random walk
+moving = ["forward", "backward"]
+corner = ["left", "right"]
+colours = ["red", "green", "yellow", "blue", "purple", "brown", "black"]
+
+def random_walk():
+    for i in range(100):
+        tono.pencolor(random.choice(colours))
+        random_move = random.choice(moving)
+        random_corner = random.choice(corner)
+        if random_move == "forward" and random_corner == "left":
+            tono.forward(random.randint(10, 50))
+            tono.left(90)
+        elif random_move == "forward" and random_corner == "right":
+            tono.forward(random.randint(10, 50))
+            tono.right(90)
+        elif random_move == "backward" and random_corner == "left":
+            tono.backward(random.randint(10, 50))
+            tono.left(90)
+        elif random_move == "backward" and random_corner == "right":
+            tono.backward(random.randint(10, 50))
+            tono.right(90)
+    
+random_walk()
 
 screen = Screen()
 screen.exitonclick()
@@ -90,5 +118,5 @@ screen.exitonclick()
 
 # try to use package that doesn't exist before installing
 import heroes
-print(heroes.gen())
+#print(heroes.gen())
 
