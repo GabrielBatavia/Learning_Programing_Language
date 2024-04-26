b77290483fce2b621660e8791741a3f8521a6d3a
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-26 18:48:59 +0700

    just play around and do challenge 2 by making dash line with other method that tono have

--- Learning_Python/100_Day/day18/day18.py
+++ Learning_Python/100_Day/day18/day18.py
@@ -1,18 +1,30 @@
 from turtle import Turtle, Screen
 
-tono_the_turtle = Turtle()
-tono_the_turtle.shape("turtle")
-tono_the_turtle.color("green", "yellow")
+tono = Turtle()
+tono.shape("turtle")
+tono.color("green", "yellow")
 
 # challenge 1 ## make a square
-tono_the_turtle.forward(100)
-tono_the_turtle.left(90)
-tono_the_turtle.forward(100)
-tono_the_turtle.left(90)
-tono_the_turtle.forward(100)
-tono_the_turtle.left(90)
-tono_the_turtle.forward(100)
-tono_the_turtle.left(90)
+def make_square():
+    for i in range(0, 4):
+        tono.forward(100)
+        tono.left(90)
+
+# challenge 2
+def make_dash_line():
+    for i in range(10):
+        tono.forward(10)
+        tono.pd()
+        tono.forward(10)
+        tono.pu()
+    
+make_dash_line()
 
 screen = Screen()
 screen.exitonclick()
+
+
+# try to use package that doesn't exist before installing
+import heroes
+print(heroes.gen())
+
