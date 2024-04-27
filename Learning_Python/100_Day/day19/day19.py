b5d0be6096e993ef8e0c5c370e1be718aa5b9213
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-27 13:38:12 +0700

    add clear function that will restart our drawing line

--- Learning_Python/100_Day/day19/day19.py
+++ Learning_Python/100_Day/day19/day19.py
@@ -14,10 +14,17 @@ def move_left():
 
 def move_right():
     tono.right(10)
+    
+def clear():
+    tono.clear()
+    tono.penup()
+    tono.home()
+    tono.pendown()
 
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=move_left)
 screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
