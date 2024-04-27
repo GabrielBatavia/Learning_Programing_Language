c58266300f81dc827b070175a0afd74bb27c56c6
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-27 13:25:12 +0700

    add funtion and triger to make an Etch-A-Sketch App

--- Learning_Python/100_Day/day19/day19.py
+++ Learning_Python/100_Day/day19/day19.py
@@ -6,6 +6,18 @@ screen = Screen()
 def move_forwards():
     tono.forward(10)
 
+def move_backwards():
+    tono.backward(10)
+
+def move_left():
+    tono.left(10)
+
+def move_right():
+    tono.right(10)
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
 screen.exitonclick()
