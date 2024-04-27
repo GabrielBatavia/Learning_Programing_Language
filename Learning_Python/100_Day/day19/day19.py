09caa7dffa6faec95b00830e64b034b8c719b99b
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-27 16:48:48 +0700

    add the code for making all our turtle ready for the start of the race

--- Learning_Python/100_Day/day19/day19.py
+++ Learning_Python/100_Day/day19/day19.py
@@ -1,30 +1,60 @@
 from turtle import Turtle, Screen
 
-tono = Turtle()
 screen = Screen()
+screen.setup(width=500,height=400)     # set up scrre
 
-def move_forwards():
-    tono.forward(10)
+# make the pop up screen
+user_bet = screen.textinput(title="Make your bet", prompt="Which tutle win the race? Enter your color choose : ")
+print(f"You bet a {user_bet}")
+colors = ["red", "green", "blue", "yellow", "purple", "orange"]
+Y_post = [-70, -40, -10, 20, 50, 80]
 
-def move_backwards():
-    tono.backward(10)
-
-def move_left():
-    tono.left(10)
-
-def move_right():
-    tono.right(10)
-    
-def clear():
-    tono.clear()
+for turtle_index in range(0, 6):
+    tono = Turtle(shape="turtle")
+    tono.color(colors[turtle_index])
     tono.penup()
-    tono.home()
-    tono.pendown()
+    tono.goto(x=-230, y=Y_post[turtle_index])
+
+
+
+#def move_forwards():
+#    tono.forward(10)
+
+#def move_backwards():
+#    tono.backward(10)
+
+#def move_left():
+#    tono.left(10)
+
+#def move_right():
+#    tono.right(10)
+    
+#def clear():
+#    tono.clear()
+#    tono.penup()
+#    tono.home()
+#    tono.pendown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=move_left)
-screen.onkey(key="d", fun=move_right)
-screen.onkey(key="c", fun=clear)
+#screen.onkey(key="w", fun=move_forwards)
+#screen.onkey(key="s", fun=move_backwards)
+#screen.onkey(key="a", fun=move_left)
+#screen.onkey(key="d", fun=move_right)
+#screen.onkey(key="c", fun=clear)
 screen.exitonclick()
