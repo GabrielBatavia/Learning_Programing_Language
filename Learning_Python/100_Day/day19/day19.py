780d6e350ce2022054cf1017559ff69205dcafee
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-27 17:00:58 +0700

    add code for all the turtle move forward and it ready for the race

--- Learning_Python/100_Day/day19/day19.py
+++ Learning_Python/100_Day/day19/day19.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500,height=400)     # set up scrre
@@ -8,14 +9,25 @@ user_bet = screen.textinput(title="Make your bet", prompt="Which tutle win the r
 print(f"You bet a {user_bet}")
 colors = ["red", "green", "blue", "yellow", "purple", "orange"]
 Y_post = [-70, -40, -10, 20, 50, 80]
+is_race_on = False
+all_turtles = []
 
 for turtle_index in range(0, 6):
-    tono = Turtle(shape="turtle")
-    tono.color(colors[turtle_index])
-    tono.penup()
-    tono.goto(x=-230, y=Y_post[turtle_index])
+    new_tono = Turtle(shape="turtle")
+    new_tono.color(colors[turtle_index])
+    new_tono.penup()
+    new_tono.goto(x=-230, y=Y_post[turtle_index])
+    all_turtles.append(new_tono)
 
 
+if user_bet:
+    is_race_on = True
+    
+while is_race_on:
+    
+    for turtle in all_turtles:
+        random_move = random.randint(0, 10)
+        turtle.forward(random_move)
 
 #def move_forwards():
 #    tono.forward(10)
