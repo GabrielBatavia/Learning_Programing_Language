5822590b7419281faf1b8923a6434f5029ae30f6
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-28 07:47:55 +0700

    start day20 and start to make snake nokia game project

---
+++ Learning_Python/100_Day/day20/main_snake.py
@@ -0,0 +1,43 @@
+from turtle import Turtle, Screen
+
+# setting the screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# set up the exit screen in bottom of our code
+screen.exitonclick()
