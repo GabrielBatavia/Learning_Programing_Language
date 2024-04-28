964c6c259e7b823c499774cefea53e6c74215500
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-28 13:07:48 +0700

    modify our snake body so its get right logic for the next part

--- Learning_Python/100_Day/day20/main_snake.py
+++ Learning_Python/100_Day/day20/main_snake.py
@@ -1,4 +1,6 @@
 from turtle import Turtle, Screen
+import time
+game_on = True
 
 # setting the screen
 screen = Screen()
@@ -6,11 +8,29 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+# set the snake
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
 
 # create the snake body
-snake = Turtle(shape="square")
-snake.shapesize(stretch_len=3)
-snake.color("white")
+for position in starting_positions:
+    snake = Turtle(shape="square")
+    snake.color("white")
+    snake.up()
+    snake.goto(position)
+    segments.append(snake)
+
+while game_on:
+    for seg in segments:
+        seg.forward(100)
+        seg.left(90)
+        screen.update()
+        time.sleep(1)
+
+
+
 
 
 
