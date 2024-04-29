28d6c2f681fa5e9aed19f333cbde16b2ad0b77c9
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-29 08:16:53 +0700

    modify our code by using oop concept to snake

--- Learning_Python/100_Day/day20/main_snake.py
+++ Learning_Python/100_Day/day20/main_snake.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
+from Snake import Snake
 game_on = True
 
 # setting the screen
@@ -10,29 +11,14 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 # set the snake
+snake = Snake()
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-# create the snake body
-for position in starting_positions:
-    snake = Turtle(shape="square")
-    snake.color("white")
-    snake.up()
-    snake.goto(position)
-    segments.append(snake)
 
 while game_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) -1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    
-    segments[0].forward(20)
+    snake.move()
 
 
 
