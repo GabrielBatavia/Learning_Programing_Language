8a5e442cd8e68d9102595aa653cd2f8658883373
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-30 08:07:42 +0700

    continue day 21 and add food class

---
+++ Learning_Python/100_Day/day20_day21/food.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+
+# create food class
+class Food(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        
+        random_x = random_randint(-280, 280)
+        random_y = random_randint(-280, 280)
+        self.goto(random_x, random_y)
