ccbf4ab9665d75eaff9ec0e97fba9f39460a3bfe
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-01 08:49:26 +0700

    Create a Paddle that responds to Key Presses

--- Learning_Python/100_Day/day22/Pong_main.py
+++ Learning_Python/100_Day/day22/Pong_main.py
@@ -1,13 +1,37 @@
 from turtle import Turtle, Screen
-
-pong = Turtle()
-
+game_is_on = True
 
 # set up screen
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("PONG GAME")
+screen.tracer(0)
+
+# set up the paddle
+paddle = Turtle("square")
+paddle.color("white")
+paddle.penup()
+paddle.shapesize(stretch_wid=3, stretch_len=0.5)
+paddle.goto(350, 0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+# make a move
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+# set up main 
+while game_is_on:
+    screen.update()
 
 
 
