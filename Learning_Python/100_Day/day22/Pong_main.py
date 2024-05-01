89b42dd775bf252f92bf24c61571caddddb2ef28
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-01 08:29:56 +0700

    start day 22 and start create pong game project, now we setup the main screen

---
+++ Learning_Python/100_Day/day22/Pong_main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+pong = Turtle()
+
+
+# set up screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG GAME")
+
+
+
+
+
+
+
+
+
+
+# set up the exit screen in bottom of our code
+screen.exitonclick()
