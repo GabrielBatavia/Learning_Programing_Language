317a297808954643b61c3ee8256ff7905ee396ce
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-03 20:25:38 +0700

    make a lopp and using os library inside the loop for open and save the image that we choice

--- Learning_Python/prepeare/Pillow/pil.py
+++ Learning_Python/prepeare/Pillow/pil.py
@@ -1,5 +1,16 @@
 # import pillow
 from PIL import Image
+import os
 
 image1 = Image.open("./img/img_1.png")
-image1.show()
+#image1.show() # to open and show image
+
+# we can save the image
+#image1.save('rumah.png') # its will create a new image with the image
+
+for f in os.listdir('.'):
+    if f.endswith('.png'):
+        i = Image.open(f)
+        fn, fext = os.path.splitext(f)
+        i.save('pngs/{}.png'.format(fn))
+        
