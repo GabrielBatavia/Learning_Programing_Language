dd3141aebe03fe957cd900a42f5a035d3b998fec
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-03 11:56:26 +0700

    learn open with 'with' way

--- Learning_Python/100_Day/day24/day24.py
+++ Learning_Python/100_Day/day24/day24.py
@@ -1,11 +1,16 @@
 # try to open the file
-file = open("my_file.txt")
-content = file.read()
+#file = open("my_file.txt")
+#content = file.read()
 
-print(content)
-
-
-
+#print(content)
 
 # close the file to minimize memory usage
-file.close()
+#file.close()
+
+
+## Another way with better performance
+# with this way, we dont have to worry about close syntax
+with open("my_file.txt") as file: 
+    content = file.read()
+    print(content)
+    
