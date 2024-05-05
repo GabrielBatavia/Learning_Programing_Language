09d94c3aa984ff2832c9a1e40c442c420148ae90
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-05 16:08:11 +0700

    learn how to use pandas little bit and convert to another type

--- Learning_Python/100_Day/day25/day25.py
+++ Learning_Python/100_Day/day25/day25.py
@@ -35,4 +35,22 @@ print()
 import pandas as pd
 
 data = pd.read_csv('weather_data.csv')
+print(type(data)) # pandas frame == table
 print(data)
+print()
+print(type(data["temp"])) # pandas series == column
+print(data["temp"])
+
+print()
+
+# convert to dictionary
+data_dict = data.to_dict()
+print(type(data_dict))
+print(data_dict)
+
+print()
+
+# convert to list
+temp_list = data["temp"].to_list()
+print(type(temp_list))
+print(temp_list)
