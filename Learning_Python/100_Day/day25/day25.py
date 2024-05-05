d2a143d9e40dd81bbf998cda9e943beb57e08b85
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-05 15:04:19 +0700

    open using csv library and open with pandas library

--- Learning_Python/100_Day/day25/day25.py
+++ Learning_Python/100_Day/day25/day25.py
@@ -1,4 +1,38 @@
-with open('weather_data.csv') as data_file:
-    data = data_file.readlines()
+# reguler way
+#with open('weather_data.csv') as data_file:
+#    data = data_file.readlines()
 
+#   print(data)
+
+
+# using csv library
+import csv
+
+tempratures = []
+
+with open('weather_data.csv') as data_file:
+    data = csv.reader(data_file)
+    
+    for row in data:
+        print(row)
+        
+    # kembali ke awal file csv
+    data_file.seek(0) # ini dilakukan jika kita sudah melakukan sesuatu ke data namun ingin melakukan hal lain lagi
+    
+    for row in data:
+        if row[1] != "temp": # exclude label 
+            tempratures.append(row[1])
+    
+    print(tempratures)
+    
+
+print()
+print()
+
+# use the more useful library
+# pandas is powerful library
+
+import pandas as pd
+
+data = pd.read_csv('weather_data.csv')
 print(data)
