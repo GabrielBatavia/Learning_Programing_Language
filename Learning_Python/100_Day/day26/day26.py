93b910ec4c0b8afc838ad7cfaa3d1be4ed423dcc
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-07 05:09:43 +0700

    learn how to use dictionary comprehension

--- Learning_Python/100_Day/day26/day26.py
+++ Learning_Python/100_Day/day26/day26.py
@@ -38,3 +38,22 @@ names = ["Shalom", "Linda", "Syafa", "Nia", "Putri"]
 friends = [name.upper() for name in names if len(name) > 4]
 
 print(friends)
+
+
+# Dictionary Comprehension
+## new_dictionary = {new_key:new_value for (key, value) in dict.items()}
+
+# Conditional dictionary comprehension
+## new_dictionary = {new_key:new_value for (key, value) in dict.items() if test}
+
+import random
+
+names = ["Shalom", "Linda", "Syafa", "Nia", "Putri"]
+student_score = {student:random.randint(1,100) for student in names}
+
+print(student_score)
+print()
+
+passed_student = {student:score for (student, score) in student_score.items() if score > 60}
+
+print(passed_student)
