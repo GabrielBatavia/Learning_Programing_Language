b4dd05962c4f2e3d9461f7b0c9994483370854ec
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-10 12:16:57 +0700

    start day 30 and learn catching exceptions code

---
+++ Learning_Python/100_Day/day30/day30.py
@@ -0,0 +1,40 @@
+# all errors
+
+# FilenotFound
+# with open("asdsa.txt") as file:
+#    file.read()
+
+# Key Error
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_exixstent_key"]
+
+#IndexError
+# fruit_list = ["apple"]
+# fruit = fruit_list[3]
+
+# Typeerror
+# text = "abc"
+# print(text + 4)
+
+
+# Catching exceptions
+
+try:
+    with open("asdsa.txt") as file:
+        file.read()
+    a_dictionary = {"key": "value"}
+    value = a_dictionary["non_exixstent_key"]
+
+except FileNotFoundError:
+    with open("asdsa.txt", mode="w") as file:
+        file.write("asdsa.txt")
+
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
+
+else:
+    print("The file open is successful")
+
+finally:
+    print("We are done")
+    
