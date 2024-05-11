42f25e099a9648ce9130248b8835af515cae0bd8
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-11 14:29:33 +0700

    read csv and creating new flash cards function

--- Learning_Python/100_Day/day31/main.py
+++ Learning_Python/100_Day/day31/main.py
@@ -2,12 +2,22 @@
 
 from tkinter import *
 from tkinter import messagebox
-import random
+import pandas as pd
 import pyperclip
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+data = pd.read_csv("./id_english_words.csv")
+to_learn = data.to_dict(orient="records")
 
 
+# ---------------------------- ALL FUNCTION ------------------------------- #
+
+def next_card():
+    current_card = random.choice(to_learn)
+    english_word = current_card["inEnglish"]
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=english_word)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -19,14 +29,14 @@ card_front_img = PhotoImage(file='./images/card_front.png')
 canvas.create_image(400, 263, image=card_front_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 
 canvas.grid(column=0, row=0, columnspan=2)
 
 
 cross_image = PhotoImage(file="./images/wrong.png")
-unknown_button = Button(image=cross_image)
+unknown_button = Button(image=cross_image, command=next_card)
 unknown_button.config(bg=BACKGROUND_COLOR ,highlightthickness=0)
 unknown_button.grid(row=1, column=0)
 
@@ -35,6 +45,6 @@ know_button = Button(image=check_image)
 know_button.config(bg=BACKGROUND_COLOR ,highlightthickness=0)
 know_button.grid(row=1, column=1)
 
-
+next_card()
 
 window.mainloop()
