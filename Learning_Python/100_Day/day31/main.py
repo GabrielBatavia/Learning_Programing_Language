80becbee8f44eedd98dd69362a12b55e0b86dd26
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-11 14:46:40 +0700

    completed code for the main project

--- Learning_Python/100_Day/day31/main.py
+++ Learning_Python/100_Day/day31/main.py
@@ -9,24 +9,39 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 data = pd.read_csv("./id_english_words.csv")
 to_learn = data.to_dict(orient="records")
-
+current_card = {}
 
 # ---------------------------- ALL FUNCTION ------------------------------- #
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     english_word = current_card["inEnglish"]
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=english_word)
+    canvas.itemconfig(card_title, text="English", fill="black")
+    canvas.itemconfig(card_word, text=english_word, fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(5000, func=flip_card)
+    
+
+def flip_card():
+    Indonesia_word = current_card["Indonesian"]
+    canvas.itemconfig(card_title, text="Indonesia", fill="white")
+    canvas.itemconfig(card_word, text=Indonesia_word, fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(5000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file='./images/card_front.png')
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file='./images/card_back.png')
+card_background = canvas.create_image(400, 263, image=card_front_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
@@ -41,7 +56,7 @@ unknown_button.config(bg=BACKGROUND_COLOR ,highlightthickness=0)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="./images/right.png")
-know_button = Button(image=check_image)
+know_button = Button(image=check_image, command=next_card)
 know_button.config(bg=BACKGROUND_COLOR ,highlightthickness=0)
 know_button.grid(row=1, column=1)
 
