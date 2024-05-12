c70fc3863d30602e02d465d4e61666a755c8fd0a
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-12 21:00:09 +0700

    make program to send motivational Quotes via email

--- Learning_Python/100_Day/day32/main.py
+++ Learning_Python/100_Day/day32/main.py
@@ -1,25 +1,26 @@
-#import smtplib
-
-#my_email = "xaveriusgabriel10@gmail.com"
-#password = ""
-#to_email = "bataviasensei@gmail.com"
-
-# Message we want to send
-#subject = "Hello from Python"
-#body = "This is a test email sent from Python."
-#message = f"Subject: {subject}\n\n{body}"
-
-#with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#    connection.starttls()
-#    connection.login(user=my_email, password=password)
-#    connection.sendmail(from_addr=my_email, to_addrs=to_email, msg=message)
-
-
+import smtplib
 import datetime as dt
+import random
+
+my_email = "xaveriusgabriel10@gmail.com"
+password = ""
+to_email = "petruskmkz@gmail.com"
 
 now = dt.datetime.now()
-year = now.year
-#print(year)
+weekday = now.weekday()
 
-date_of_birth = dt.datetime(year=2005, month=5, day=12, hour=15)
-print(date_of_birth)
+
+if weekday == 0:
+    with open("./quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+        
+    # Message we want to send
+    subject = "Hello from Gabriel Batavia"
+    body = f"Hay...remember...im always care about you...this is quote for you today : {quote}"
+    message = f"Subject: {subject}\n\n{body}"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=to_email, msg=message)
