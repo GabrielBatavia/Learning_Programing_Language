21f23c25aae33de07372417d139f622542dc4d0f
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-13 20:18:50 +0700

    completed kanye project and making the program using kanye quote api

--- Learning_Python/100_Day/day33/Kanye_Project/main.py
+++ Learning_Python/100_Day/day33/Kanye_Project/main.py
@@ -1,15 +1,18 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
-
-
-
+    response = requests.get(url="https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
+    
 window = Tk()
 window.title("Kanye Says...")
 window.config(padx=50, pady=50)
+
 
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="./Kanye_Project/background.png")
