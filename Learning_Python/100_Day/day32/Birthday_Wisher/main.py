0a88e6f29722c8b2b66826659439b37959072616
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-13 08:31:40 +0700

    completed birthday_wisher project

--- Learning_Python/100_Day/day32/Birthday_Wisher/main.py
+++ Learning_Python/100_Day/day32/Birthday_Wisher/main.py
@@ -1,22 +1,34 @@
 ##################### Hard Starting Project ######################
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
+import datetime as dt
+import pandas as pd
+import random
+import smtplib
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+MY_EMAIL = "xaveriusgabriel10@gmail.com"
+PASSWORD = ""
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+today = (dt.datetime.now().month, dt.datetime.now().day)
+data = pd.read_csv("./Birthday_Wisher/birthdays.csv")
 
 
+birth_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
+if today in birth_dict:
+    person = birth_dict[today]
+    
+    file_path = f"./Birthday_Wisher/letter_templates/letter_{random.randint(1,3)}.txt"
+    
+    with open(file_path) as template:
+        content = template.read()
+        content = content.replace("[NAME]", person["name"])
+        message = content
+    
+    to_email = person["email"]
+        
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=to_email, msg=message)
+    
+    
---
+++ Learning_Python/100_Day/day32/Birthday_Wisher/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+/letter_templates/letter_{random.randint(1,3)}.txt"
+    
