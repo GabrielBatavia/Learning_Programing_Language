32882b3c1d5540ea440db55bfe567ce863102875
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-15 17:53:56 +0700

    give feedback to player, keep score in gui dan fix bugs

--- Learning_Python/100_Day/day34/Trivia_Quiz_Project/ui.py
+++ Learning_Python/100_Day/day34/Trivia_Quiz_Project/ui.py
@@ -15,7 +15,7 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
         
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, width=200,text="Some Question", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(150, 125, width=200,text="Some Question", fill=THEME_COLOR, font=("Arial", 15, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=30)
         
         self.true_image = PhotoImage(file="./Trivia_Quiz_Project/images/true.png")
@@ -32,11 +32,27 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        
+        if self.quiz.still_has_questions(): 
+            self.score_label.config(text=f"Score : {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You're reached the ned of the Quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
         
     def true_pressed(self):
-        self.quiz.check_answer("True")
-    
+        self.give_feedback(self.quiz.check_answer("True"))
+        
     def false_pressed(self):
-        self.quiz.check_answer("False")
+        self.give_feedback(self.quiz.check_answer("False"))
+        
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+            
+        self.window.after(1000,self.get_next_question)
