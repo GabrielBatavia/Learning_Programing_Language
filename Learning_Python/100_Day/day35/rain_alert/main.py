5051148a134e35a6658c54d9a92ba2ea0390040a
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-16 11:57:35 +0700

    add code to check if it will rain in the next 12 hours in Jakarta

--- Learning_Python/100_Day/day35/rain_alert/main.py
+++ Learning_Python/100_Day/day35/rain_alert/main.py
@@ -1,13 +1,30 @@
 import requests
 from datetime import datetime
 
-MY_LAT = -7.966620
-MY_LNG = 112.632629
+MY_LAT = -6.175110
+MY_LNG = 106.865036
 
 api_key = "551bfd1aedf3be81762a1a1835da73bd"
 
-response = requests.get(url=f"https://api.openweathermap.org/data/2.5/weather?lat={MY_LAT}&lon={MY_LNG}&appid={api_key}")
+weather_params = {
+    "lat": MY_LAT,
+    "lon": MY_LNG,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(url=f"https://api.openweathermap.org/data/2.5/forecast", params=weather_params)
 response.raise_for_status()
 
-data = response.json()
-print(data)
+weather_data = response.json()
+# weather_data["list"][0]["weather"]
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    
+    if int(condition_code) < 700:
+        will_rain = True
+    
+if will_rain:
+    print("Bring an umbrella")
