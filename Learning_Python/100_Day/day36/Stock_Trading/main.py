42118e7fec7d93823680d2a5ae7b5f1e730b08fb
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-17 21:05:21 +0700

    modify and imprve our sms format and finish the project

--- Learning_Python/100_Day/day36/Stock_Trading/main.py
+++ Learning_Python/100_Day/day36/Stock_Trading/main.py
@@ -20,11 +20,6 @@ parameters = {
     "apikey": STOCK_API_KEY
 }
 
-## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 
 response = requests.get(url=STOCK_ENDPOINT, params=parameters)
 response.raise_for_status()
@@ -37,27 +32,23 @@ yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data['4. close']
 print(yesterday_closing_price)
 
-#TODO 2. - Get the day before yesterday's closing stock price
+
 
 before_yesterday_data = data_list[1]
 before_yesterday_closing_price = before_yesterday_data['4. close']
 print(before_yesterday_closing_price)
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference_price = float(before_yesterday_closing_price) - float(yesterday_closing_price)
-difference_price = abs(difference_price)
-print(difference_price)
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference_price / float(yesterday_closing_price)) * 100
-print(diff_percent)
+if difference_price > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+diff_percent = round((difference_price / float(yesterday_closing_price)) * 100)
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-if diff_percent > 0:
+if abs(diff_percent) > 0:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
@@ -69,7 +60,7 @@ if diff_percent > 0:
     three_articles = articles[:3]
     print(three_articles)
     
-    formatted_articles = [f"Headline: {article['title']}. \n Brief: {article['description']}" for article in three_articles]
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}% \nHeadline: {article['title']}. \n Brief: {article['description']}" for article in three_articles]
     
     for article in formatted_articles:
         client = Client(account_sid, auth_token)
@@ -79,15 +70,4 @@ if diff_percent > 0:
             to = "+6285157677512"
         )
 
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
