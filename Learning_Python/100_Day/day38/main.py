6674f9300b92dc6647b7a5da0a9e061fde83fb9c
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-19 20:30:44 +0700

    use sheety api and connect to our google spreadsheets (still not working for post cayse authorize setting in our gmail account)

--- Learning_Python/100_Day/day38/main.py
+++ Learning_Python/100_Day/day38/main.py
@@ -1,4 +1,5 @@
 import requests
+import datetime as dt
 
 GENDER = "male"
 WEIGHT_KG = "100"
@@ -7,6 +8,7 @@ AGE = "19"
 
 APP_ID = "c69ec56b"
 API_KEY = "e8799c74ae966fa89c6901c002151040"
+SHETTY_ENDPOINT = "https://api.sheety.co/5fb410767ff998e7fb4d26150f7a67dd/bataviaWorkouts/workouts"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -29,4 +31,31 @@ parameters = {
 
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
-print(result)
+#print(result)
+
+
+
+
+# sheet #
+
+today_date = dt.datetime.now().strftime("%d/%m/%Y")
+now_time = dt.datetime.now().strftime("%X")
+
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    #sheet_response = requests.post(SHETTY_ENDPOINT, json=sheet_inputs)
+
+    #print(sheet_response.text)
+    
+sheet_response = requests.get(SHETTY_ENDPOINT, headers=headers, verify=False)
+print(sheet_response.text)
