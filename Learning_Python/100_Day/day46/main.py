8fc67c747475976a0367654a5ab45f846b217925
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-25 15:49:43 +0700

    modify the function and make it works by using user input

--- Learning_Python/100_Day/day46/main.py
+++ Learning_Python/100_Day/day46/main.py
@@ -1,6 +1,5 @@
 import requests as req
 from bs4 import BeautifulSoup
-URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
 
 # Function to scrape song titles from a given Billboard Hot 100 URL
 def scrape_song_titles(url):
@@ -9,14 +8,24 @@ def scrape_song_titles(url):
 
     soup = BeautifulSoup(website_html, "html.parser")
 
-    # Finding the song title elements
-    song_title_elements = soup.find_all("span", class_="chart-element__information__song")
+    # Finding the song title elements using the correct selector
+    song_title_elements = soup.select("li.lrv-u-width-100p h3")
 
     # Extracting the text (song titles) from the elements
-    song_titles = [song.get_text() for song in song_title_elements]
+    song_titles = [song.get_text(strip=True) for song in song_title_elements]
 
     return song_titles
 
+# Prompting the user for a date to travel
+date = input("Which time you want to travel? Type the date in this format YYYY-MM-DD: ")
 
-input("Which time you want to travel? Type the data in this format YYYY-MM-DD : ")
+# Constructing the URL based on the user input date
+URL = f"https://www.billboard.com/charts/hot-100/{date}/"
 
+# Scraping the song titles
+song_titles = scrape_song_titles(URL)
+
+# Printing the list of song titles
+print("Song titles on Billboard Hot 100 for the selected date:")
+for title in song_titles:
+    print(title)
