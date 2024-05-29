e1871fb2e13d6102301403fafaee2d7dffaa61ec
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-29 08:50:07 +0700

    Update web scraping script to write output to file
    - Enhanced script to save scraped data to joko_widodo.txt
    - Created a new directory 'Selenium_Scrape' for the output file
    - Writes the figure name and all paragraphs to the text file
    - Ensures the output file is encoded in UTF-8

--- Learning_Python/100_Day/day48/Selenium_Scrape/interaction.py
+++ Learning_Python/100_Day/day48/Selenium_Scrape/interaction.py
@@ -1,13 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# Keep edge browser open after open
+# Keep Edge browser open after opening
 edge_options = webdriver.EdgeOptions()
 edge_options.add_experimental_option("detach", True)
 
-
 URL = "https://id.wikipedia.org/wiki/Joko_Widodo"
-
 
 driver = webdriver.Edge(options=edge_options)
 driver.get(URL)
@@ -15,13 +13,15 @@ driver.get(URL)
 # Find the element containing the name
 figure_name_element = driver.find_element(By.CLASS_NAME, "mw-page-title-main")
 figure_name = figure_name_element.text
-print(figure_name)
-
 
 # Find all paragraph elements
 paragraph_elements = driver.find_elements(By.TAG_NAME, "p")
-for paragraph in paragraph_elements:
-    print(paragraph.text)
+
+# Write the figure name and paragraphs to a file
+with open("./Selenium_Scrape/joko_widodo.txt", "w", encoding="utf-8") as file:
+    file.write(figure_name + "\n\n")
+    for paragraph in paragraph_elements:
+        file.write(paragraph.text + "\n\n")
 
 # End browser
 driver.quit()
