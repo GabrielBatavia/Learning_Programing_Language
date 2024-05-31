da5952aed156a043e1a26210e6147d0a54a7879e
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-31 17:45:05 +0700

    Add LinkedIn job search automation script
    - Implement dotenv to load environment variables
    - Configure Chrome WebDriver with options
    - Automate login to LinkedIn using stored credentials
    - Navigate to specific job search URL
    - Perform job search and save job functionality

--- Learning_Python/100_Day/day49/main.py
+++ Learning_Python/100_Day/day49/main.py
@@ -14,11 +14,15 @@ chrome_options.add_experimental_option("detach", True)
 EMAIL = os.getenv('EMAIL')
 PASSWORD = os.getenv('PASSWORD')
 
-URL = "https://www.linkedin.com/feed/"
+URL = "https://www.linkedin.com/jobs/search/?currentJobId=3936718501&f_AL=true&f_E=1&geoId=101165590&keywords=python&location=United%20Kingdom&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true"
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
+time.sleep(5)
 
+# Sign in button
+sign_in_button = driver.find_element(By.CSS_SELECTOR, "a.nav__button-secondary.btn-md.btn-secondary-emphasis")
+sign_in_button.click()
 
 # Sign in
 time.sleep(5)
@@ -29,4 +33,13 @@ password_field.send_keys(PASSWORD)
 
 sign_in_button = driver.find_element(by=By.CSS_SELECTOR, value="button[data-litms-control-urn='login-submit']")
 sign_in_button.click()
+
+time.sleep(30)
+
+job = driver.find_element(By.CSS_SELECTOR, "a[data-control-name='job_card']")
+job.click()
+
+save_job = driver.find_element(by=By.CSS_SELECTOR, value="button.jobs-save-button")
+save_job.click()
+
 
