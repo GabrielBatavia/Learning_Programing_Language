c6f5dc7261ce75e011f92e142221570974481817
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-05-31 16:10:50 +0700

    feat: Automate LinkedIn login with Selenium
    - Added script to automate login process for LinkedIn using Selenium.
    - Implemented environment variable loading for credentials.
    - Included explicit waits to ensure elements are loaded before interaction.
    - Improved element selection for reliable automation.

--- Learning_Python/100_Day/day49/main.py
+++ Learning_Python/100_Day/day49/main.py
@@ -1,7 +1,12 @@
 import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
+
+load_dotenv()
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -9,10 +14,19 @@ chrome_options.add_experimental_option("detach", True)
 EMAIL = os.getenv('EMAIL')
 PASSWORD = os.getenv('PASSWORD')
 
-URL = "https://www.linkedin.com/feed/?trk=IN-SEM_google-adwords_Jordan-brand-sign-up"
+URL = "https://www.linkedin.com/feed/"
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
-login_email = driver.find_elements(By.ID, value="username")
-login_email.send_keys("")
+
+# Sign in
+time.sleep(5)
+email_field = driver.find_element(by=By.ID, value="username")
+email_field.send_keys(EMAIL)
+password_field = driver.find_element(by=By.ID, value="password")
+password_field.send_keys(PASSWORD)
+
+sign_in_button = driver.find_element(by=By.CSS_SELECTOR, value="button[data-litms-control-urn='login-submit']")
+sign_in_button.click()
+
