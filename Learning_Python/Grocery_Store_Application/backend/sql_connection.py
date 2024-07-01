0045df8b9b9dc90099be6a507c3003c7764666f9
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-07-01 23:13:40 +0700

    feat : remove some unimportant code

--- Learning_Python/Grocery_Store_Application/backend/sql_connection.py
+++ Learning_Python/Grocery_Store_Application/backend/sql_connection.py
@@ -4,18 +4,8 @@ __cnx = None
 
 def get_sql_connection():
     global __cnx
-    try:
-        __cnx = mysql.connector.connect(host="localhost", user="root", password="", database="gs", port=3306)
-        return __cnx
-    except mysql.connector.Error as err:
-        print(f"Error connecting to MySQL: {err}")
-        if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-        raise
+    __cnx = mysql.connector.connect(host="localhost", user="root", password="", database="gs", port=3306)
+    return __cnx
 
 
 if __name__ == '__main__':
